ff68909e61775fb777673e64cd33c83ea72a4400
Tanish.Patel <tanishpatel0106@gmail.com>
2024-10-27 12:01:39 +0530

    contact page removed

--- src/constants/navlinks.tsx
+++ src/constants/navlinks.tsx
@@ -27,9 +27,9 @@ export const navlinks = [
     label: "Articles",
     icon: IconArticle,
   },
-  {
-    href: "/contact",
-    label: "Contact",
-    icon: IconMail,
-  },
+  // {
+  //   href: "/contact",
+  //   label: "Contact",
+  //   icon: IconMail,
+  // },
 ];
