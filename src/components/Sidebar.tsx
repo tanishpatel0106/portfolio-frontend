ac570636d41f264e00a8c4795941be16c9954cb1
Tanish <tanishpatel0106@gmail.com>
2026-03-21 16:59:55 -0400

    Professional headshot added to the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -109,7 +109,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="/images/photo.png"
+        src="/images/Headshot.jpeg"
         alt="Avatar"
         height="40"
         width="40"
